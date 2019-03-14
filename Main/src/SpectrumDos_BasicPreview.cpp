716375b553186f6796f86e4351e5d17f3b45e8de
tomascz <nestorovic@seznam.cz>
2019-03-14 09:27:22 +0100

    [#29] All DOSes: Revision of file size retrieval (Part 3: Fix of GDOS and TR-DOS Basic listing)

--- Main/src/SpectrumDos_BasicPreview.cpp
+++ Main/src/SpectrumDos_BasicPreview.cpp
@@ -57,6 +57,10 @@
 		// generates HTML-formatted BASIC listing of the input File into a temporary file
 		// - creating the File reader
 		CFileReaderWriter frw(DOS,pdt->entry);
+		BYTE a,z;
+		const DWORD fileOfficialSize=DOS->GetFileSize( pdt->entry, &a, &z, CDos::TGetFileSizeOptions::OfficialDataLength );
+		frw.SetLength( a+fileOfficialSize ); // ignoring appended custom data (e.g. as in TR-DOS)
+		frw.Seek( a, CFile::begin ); // ignoring prepended custom data (e.g. as in GDOS)
 		// - opening the temporary HTML file for writing
 		class CFormattedBasicListingFile sealed:public CFile{
 			const CBasicPreview &rBasicPreview;
