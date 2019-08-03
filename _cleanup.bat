4811ba83a5de17bc3536b90b0a89845c1ae5654e
tomascz <nestorovic@seznam.cz>
2019-08-03 20:52:50 +0200

    [#13] Minor source code improvements (Part 53: Added "Debug in RAMdisk" configuration for non-SDD users)

--- _cleanup.bat
+++ _cleanup.bat
@@ -2,20 +2,21 @@ REM --- General clean-up ---
 del _.d80 _.d40 _.dsk _.ima _.img _.trd _.scl _.tap _.mgt ride.sdf
 del /a:h ride.suo
 del C:\WINDOWS\ride.ini
-rd /s /q debug ipch release "Release MFC 4.2"
+rd /s /q debug ipch release "Release MFC 4.2" "Debug in RAMdisk"
+rd /s /q r:\ride
 
 REM --- Main project clean-up ---
 cd Main
 del res\resource.aps
-rd /s /q debug ipch release "Release MFC 4.2"
+rd /s /q debug ipch release "Release MFC 4.2" "Debug in RAMdisk"
 cd..
 
 REM --- PropGrid project clean-up ---
 cd PropGrid
-rd /s /q debug ipch release "Release MFC 4.2"
+rd /s /q debug ipch release "Release MFC 4.2" "Debug in RAMdisk"
 cd..
 
 REM --- TDI project clean-up ---
 cd Tdi
-rd /s /q debug ipch release "Release MFC 4.2"
+rd /s /q debug ipch release "Release MFC 4.2" "Debug in RAMdisk"
 cd..
