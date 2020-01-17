08faaa863e311fb83ef7d217102718e22c2a819e
tomascz <nestorovic@seznam.cz>
2020-01-17 09:48:40 +0100

    [#45] All DOSes: Foundation required for disk/volume verification (Part 6: Whole disk surface verification function)

--- Main/src/DialogVerification.cpp
+++ Main/src/DialogVerification.cpp
@@ -103,3 +103,61 @@
 		}
 		return __super::WindowProc(msg,wParam,lParam);
 	}
+
+
+
+
+
+
+
+
+
+
+
+
+	UINT AFX_CDECL TVerificationFunctions::WholeDiskSurfaceVerification_thread(PVOID pCancelableAction){
+		// thread to verify if unreadable Empty Sectors on all volume Cylinders are marked in allocation table as Bad
+		const PBackgroundActionCancelable pAction=(PBackgroundActionCancelable)pCancelableAction;
+		const CVerifyVolumeDialog::TParams &vp=*(CVerifyVolumeDialog::TParams *)pAction->GetParams();
+		const PImage image=vp.dos->image;
+		pAction->SetProgressTarget( image->GetCylinderCount() );
+		const auto sectorIdAndPositionIdentity=Utils::CByteIdentity();
+		TPhysicalAddress chs;
+		for( chs.cylinder=0; chs.cylinder<image->GetCylinderCount(); chs.cylinder++ )
+			for( chs.head=0; chs.head<vp.dos->formatBoot.nHeads; chs.head++ ){
+				if (pAction->IsCancelled()) return ERROR_CANCELLED;
+				// . getting the list of standard Sectors
+				TSectorId bufferId[(TSector)-1];
+				const TSector nSectors=vp.dos->GetListOfStdSectors( chs.cylinder, chs.head, bufferId );
+				// . determining whether the Track contains some Empty Sectors
+				CDos::TSectorStatus statuses[(TSector)-1];
+				vp.dos->GetSectorStatuses( chs.cylinder, chs.head, nSectors, bufferId, statuses );
+				bool trackContainsEmptySectors=false; // assumption
+				for( TSector s=0; s<nSectors; s++ )
+					trackContainsEmptySectors|=statuses[s]==CDos::TSectorStatus::EMPTY;
+				// . if the Track contains no Empty Sectors, proceeding with the next Track
+				if (!trackContainsEmptySectors)
+					continue;
+				// . buffering Sectors from the same Track by the underlying Image, making them ready for IMMEDIATE usage
+				image->BufferTrackData( chs.cylinder, chs.head, bufferId, sectorIdAndPositionIdentity, nSectors, true );
+				// . determining healthiness of Empty Sectors
+				for( TSector s=0; s<nSectors; s++ )
+					if (statuses[s]==CDos::TSectorStatus::EMPTY){
+						chs.sectorId=bufferId[s];
+						if (!image->GetHealthySectorData(chs)){
+							TCHAR buf[120], bufId[50];
+							::wsprintf( buf, _T("On Track %d, empty sector with %s is bad but is not marked so in the FAT."), chs.GetTrackNumber(vp.dos->formatBoot.nHeads), chs.sectorId.ToString(bufId) );
+							switch (vp.ConfirmFix(buf,_T("Future data loss at stake if not marked so."))){
+								case IDCANCEL:
+									return pAction->TerminateWithError(ERROR_CANCELLED);
+								case IDNO:
+									continue;
+							}
+							vp.dos->ModifyStdSectorStatus( chs, CDos::TSectorStatus::BAD );
+						}
+					}
+				pAction->UpdateProgress(chs.cylinder);
+			}
+		pAction->UpdateProgressFinished();
+		return ERROR_SUCCESS;
+	}
