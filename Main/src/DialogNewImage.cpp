4e434e437dfd198cec8c074980d6a2e6f3e90684
tomascz <nestorovic@seznam.cz>
2021-01-07 18:19:11 +0100

    [#49] Formatting/unformatting cylinders needs revision (Part 9: Can't create images that are by design read-only, e.g. IPF images)

--- Main/src/DialogNewImage.cpp
+++ Main/src/DialogNewImage.cpp
@@ -41,7 +41,8 @@
 		if (imageProps->supportedMedia&dosProps->supportedMedia) // DOS and Image support common Media
 			if (imageProps->supportedCodecs&dosProps->supportedCodecs) // DOS and Image support common Codecs
 				if (imageProps->sectorLengthMin<=dosStdSectorLength && dosStdSectorLength<=imageProps->sectorLengthMax)
-					rLBImage.SetItemDataPtr( rLBImage.AddString(imageProps->fnRecognize(nullptr)), (PVOID)imageProps ); // Null as buffer = one Image represents only one "device" whose name is known at compile-time
+					if (std::unique_ptr<CImage>( imageProps->fnInstantiate(nullptr) )->CanBeModified()) // Null as buffer = one Image represents only one "device" whose name is known at compile-time
+						rLBImage.SetItemDataPtr( rLBImage.AddString(imageProps->fnRecognize(nullptr)), (PVOID)imageProps ); // Null as buffer = one Image represents only one "device" whose name is known at compile-time
 	}
 
 	#define REAL_DEVICE_OPTION_STRING	_T("[ Compatible physical device ]")
