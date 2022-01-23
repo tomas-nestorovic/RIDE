8669041e3b790ddfaec7fe0001d609c0418ac470
tomascz <nestorovic@seznam.cz>
2022-01-23 11:11:16 +0100

    [#59] TrackEditor: Improvements and bugfixes (Part 67.2: Adopted Google implementation of Myers diff algorithm)

--- Main/src/Diff.h
+++ Main/src/Diff.h
@@ -42,6 +42,8 @@
 		// implementation of diff algorithm from
 		// Myers, E., "An O(ND) Difference Algorithm and its Variations," Algorithmica 1(2), 1986, p.251.
 		// Adopted from
+		// https://github.com/google/diff-match-patch
+		// and structurally inspired by
 		// https://github.com/psionic12/Myers-Diff-in-c-
 		// and
 		// http://www.mathertel.de/Diff
@@ -50,63 +52,83 @@
 		const int N;
 		const T *B;
 		int max;
-		PINT fv,rv; // pointers to [0], allowing for negative indices!
+		PINT fv,rv; // pointers set to [0], allowing for negative indices!
 		PActionProgress pap;
 
-		struct TMidSnake sealed{
-			int x,y,u,v,D;
-		} GetShortestMiddleSnake(const T *a,int n,const T *b,int m) const{
-			// find and return the Shortest Middle Snake (SMS)
+		POINT GetBisectSplit(const T *a,int n,const T *b,int m) const{
+			// find and return the point at which the A and B input strings can be split for optimal recurrent processing
 			// - initialization
+			const int DMax=(m+n+1)/2;
+			for( int k=-DMax; k<=DMax; k++ )
+				fv[k] = rv[k] = -1;
+			fv[1] = rv[1] = 0;
 			const int delta=n-m;
-			fv[1]=0;
-			rv[delta+1]=n+1;
 			// - SMS
-			const bool oddDelta=(delta&1)!=0;
-			for( int D=0,DEnd=(m+n+1)/2; D<=DEnd; D++ ){
-				for( int k=-D; k<=D; k+=2 ){ // extend the forward path
-					// . find the only or better starting point
+			const bool oddDelta=(delta&1)!=0; // if the total number of characters is odd, then the front path will collide with the reverse path
+			int fkStart=0, fkEnd=0, rkStart=0, rkEnd=0; // offsets for start and end of k loop; prevent mapping of space beyond the grid
+			for( int D=0; D<DMax; D++ ){
+				// . extend the Front snake one step ahead
+				for( int k=-D+fkStart; k<=D-fkEnd; k+=2 ){
+					// : find the only or better starting point
 					int x =	k==-D  ||  k!=D && fv[k-1]<fv[k+1]
 							? fv[k+1]
 							: fv[k-1]+1;
 					int y=x-k;
-					// . find the furthest reaching forward D-path ending in diagonal K
+					// : find the furthest reaching Front D-path ending in diagonal K
 					while (x<n && y<m && a[x]==b[y])
 						x++, y++; // diagonal
 					fv[k]=x;
-					// . overlap ?
-					if (oddDelta && delta-(D-1)<=k && k<=delta+D-1)
-						if (fv[k]>=rv[k]){
-							const TMidSnake result={ rv[k], rv[k]-k, x, y, 2*D-1 };
-							return result;
+					// : evaluating where we ended up
+					if (x>n) // ran off the right of the graph?
+						fkEnd+=2;
+					else if (y>m) // ran off the bottom of the graph?
+						fkStart+=2;
+					else if (oddDelta){
+						const int rk=delta-k;
+						if (-DMax<=rk && rk<DMax && rv[rk]!=-1){
+							// mirror rx onto top-left coordinate system
+							const int rx=n-rv[rk];
+							if (x>=rx){ // overlap of the Front and Back snakes?
+								const POINT result={ x, y };
+								return result;
+							}
 						}
+					}
 				}
-				for( int k=-D+delta; k<=D+delta; k+=2 ){ // extend the reverse path
-					// . find the only or better starting point
-					int x =	k==-D+delta  ||  k!=D+delta && rv[k-1]>=rv[k+1]
-							? rv[k+1]-1
-							: rv[k-1];
+				// . extend the Back snake one step ahead
+				for( int k=-D+rkStart; k<=D-rkEnd; k+=2 ){
+					// : find the only or better starting point
+					int x =	k==-D  ||  k!=D && rv[k-1]<rv[k+1]
+							? rv[k+1]
+							: rv[k-1]+1;
 					int y=x-k;
-					// . find the furthest reaching reverse D-path ending in diagonal K
-					while (x>0 && y>0 && a[x-1]==b[y-1])
-						x--, y--; // diagonal
+					// : find the furthest reaching reverse D-path ending in diagonal K
+					while (x<n && y<m && a[n-x-1]==b[m-y-1])
+						x++, y++; // diagonal
 					rv[k]=x;
-					// . overlap ?
-					if (!oddDelta && -D<=k && k<=D)
-						if (fv[k]>=rv[k]){
-							const TMidSnake result={ x, y, fv[k], fv[k]-k, 2*D };
-							return result;
+					// : evaluating where we ended up
+					if (x>n) // ran off the left of the graph?
+						rkEnd+=2;
+					else if (y>m) // ran off the top of the graph?
+						rkStart+=2;
+					else if (!oddDelta){
+						const int fk=delta-k;
+						if (-DMax<=fk && fk<DMax && fv[fk]!=-1){
+							// mirror fx onto top-left coordinate system
+							const POINT result={ fv[fk], fv[fk]-fk };
+							if (result.x>=n-x) // overlap of the Front and Back snakes?
+								return result;
 						}
+					}
 				}
 			}
-			// - we shouldn't end up here
-			ASSERT(FALSE);
-			static constexpr TMidSnake TooManyDiffs={ 0, 0, 0, 0, -1 };
-			return TooManyDiffs;
+			// - no commonality at all
+			static constexpr POINT NoCommonality={ -1, 0 };
+			return NoCommonality;
 		}
 
 		bool GetShortestEditScript(const T *a,int n,const T *b,int m) const{
-			// the divide-and-conquer implementation of the longes common-subsequence
+			// the divide-and-conquer implementation of the longest common-subsequence
 			// - skip equal starts
 			while (n>0 && m>0 && *a==*b)
 				a++, n--,  b++, m--;
@@ -114,7 +136,7 @@
 			while (n>0 && m>0 && a[n-1]==b[m-1])
 				n--, m--;
 			// - detect complete equality
-			if ((m|n)==0)
+			if ((m|n)<=0)
 				return true;
 			// - detect Insertion ("theirs" contains some extra bits that "this" misses; must "insert" into "this")
 			if (n==0)
@@ -142,13 +164,16 @@
 					return false; // insufficient buffer
 			// - find the shortest middle snake (SMS) and length of optimal path for A and B
 			pap->UpdateProgress( a-A, TBPFLAG::TBPF_NORMAL );
-			const TMidSnake sms=GetShortestMiddleSnake( a, n, b, m );
+			const POINT &&split=GetBisectSplit( a, n, b, m );
+			if (split.x<0) // no commonality at all?
+				return	GetShortestEditScript( a,n, b,0 ) // delete A
+						&&
+						GetShortestEditScript( a,0, b,m ); // insert B
+			ASSERT( split.y>=0 );
 			// - the path is from StartX to (x,y) and (x,y) to EndX
-			return	sms.D>=0
+			return	GetShortestEditScript( a, split.x, b, split.y )
 					&&
-					GetShortestEditScript( a, sms.x, b, sms.y )
-					&&
-					GetShortestEditScript( a+sms.u, n-sms.u, b+sms.v, m-sms.v );
+					GetShortestEditScript( a+split.x, n-split.x, b+split.y, m-split.y );
 		}
 	public:
 		CDiff(const T *A,int N)
