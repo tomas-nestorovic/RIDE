489b10bbbae45a458ceaa33ab15b27383146e073
tomas-nestorovic <nestorovic@seznam.cz>
2023-01-30 12:56:01 +0100

    [#13] Minor source code improvements (Part 206: Improved solution cleaning)

--- _cleanup.bat
+++ _cleanup.bat
@@ -4,6 +4,8 @@ del /a:h ride.suo
 del C:\WINDOWS\ride.ini
 rd /s /q debug ipch release "Release MFC 4.2" "Debug in RAMdisk"
 rd /s /q r:\ipch r:\ride
+attrib -h -s /d "%~dp0\.vs"
+rd /s /q "%~dp0\.vs"
 
 REM --- Main project clean-up ---
 cd Main
