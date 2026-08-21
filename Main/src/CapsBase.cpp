a7945d5beb3239a31b22df1e7c4220b2683ed86f
tomascz <nestorovic@seznam.cz>
2026-08-21 09:34:28 +0200

    [#97] Track corrections (Part 1.1.1: Fixed)

--- Main/src/CapsBase.cpp
+++ Main/src/CapsBase.cpp
@@ -1307,7 +1307,7 @@ invalidTrack:
 						break;
 					case ERROR_EVT_VERSION_TOO_OLD:
 						//::wsprintf( msg, _T("Determined for medium using <a id=\"details\">Method %d</a>. <a id=\"compute\">Redetermine using latest Method %d</a>"), tmpPrecomp.methodVersion, CPrecompensation::MethodLatest );
-						::wsprintf( msg, _T("Determined for medium using <a id=\"details\">Method %d</a>. <a id=\"compute\">Latest Method %d BACK SOON!</a>"), tmpPrecomp.methodVersion, CPrecompensation::MethodLatest );
+						::wsprintf( msg, _T("Determined for medium using <a id=\"details\">Method %d</a>. Latest Method %d BACK SOON!"), tmpPrecomp.methodVersion, CPrecompensation::MethodLatest );
 						break;
 					case ERROR_UNRECOGNIZED_MEDIA:
 						//::wsprintf( msg, _T("Unknown medium in Drive %c.\n<a id=\"details\">Determine even so using latest Method %d</a>"), tmpPrecomp.driveLetter, CPrecompensation::MethodLatest );
